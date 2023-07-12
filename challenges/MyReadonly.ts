660a9e13b22818b0ea29d7c72c9b0e44e3adfd4b
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-07-12 06:11:44 +0330

    feat: add MyReadonly as the first challenge

---
+++ challenges/MyReadonly.ts
@@ -0,0 +1,15 @@
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P];
+};
+
+interface Todo1 {
+  title: string;
+  description: string;
+  completed: boolean;
+  meta: {
+    author: string;
+  };
+}
+type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>];
